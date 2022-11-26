c30cb47ad58d66601ca873877683c92958cf1b03
mattjacobson <matthew.g.jacobson@gmail.com>
2022-11-26 18:46:26 -0500

    new app to handle svg and plotting

---
+++ pages/api/svg.tsx
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import parseHtml from "../../utils/parseHtml";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<String>
+) {
+  const { contractAddress, tokenId, uri } = req.query;
+  if (
+    typeof contractAddress === "string" &&
+    typeof tokenId === "string" &&
+    typeof uri === "string"
+  ) {
+    res.send(await parseHtml("svg", contractAddress, tokenId, uri));
+  }
+}
---
+++ utils/parseHtml.tsx
@@ -0,0 +1,46 @@
+import { parse } from "node-html-parser";
+
+export default async function parseHtml(
+  type: string,
+  contractAddress: string,
+  tokenId: string,
+  uri: string
+): Promise<string> {
+  const htmlResponse = await fetch(
+    `${decodeURI(uri)}/${contractAddress.toString().toLowerCase()}/${tokenId}`
+  );
+  let html = await htmlResponse.text();
+
+  let root = parse(html);
+
+  let tokenData = root
+    .getElementsByTagName("script")
+    .filter((n) => n.text.startsWith("let tokenData ="))
+    .pop()
+    ?.childNodes.pop();
+  tokenData?.parentNode.set_content(
+    tokenData.rawText.replace(
+      "let tokenData = {",
+      'let tokenData ={"plot":true,'
+    )
+  );
+
+  let body = root.querySelector("body");
+  if (!body) body = root.appendChild(parse(`<body></body>`));
+
+  if (type == "svg") {
+    body.insertAdjacentHTML(
+      "afterbegin",
+      `<script>const observer=new MutationObserver(e=>{e.forEach(e=>{e.addedNodes.forEach(e=>{if("svg"===e.tagName){e.setAttribute("xmlns","http://www.w3.org/2000/svg");let t=e.outerHTML,o=new Blob([t],{type:"plain/text"}),r=document.createElement("a"),d=URL.createObjectURL(o);r.href=d,r.download="${contractAddress}-${tokenId}.media",document.body.appendChild(r),r.click(),setTimeout(function(){document.body.removeChild(r),window.URL.revokeObjectURL(d)},0)}})})});observer.observe(document.body,{childList:!0,attributes:!1,subtree:!1});</script>`
+    );
+  } else if (type == "plot") {
+    body.insertAdjacentHTML(
+      "afterbegin",
+      `<div id='app'></div><script type="text/javascript" src="/saxi/plot.js"></script><script>const observer=new MutationObserver(e=>{e.forEach(e=>{e.addedNodes.forEach(e=>{if("svg"===e.tagName){e.setAttribute("xmlns","http://www.w3.org/2000/svg");let t=e.outerHTML,r='<?xml version="1.0" standalone="no"?>\\r\\n',o=new Blob([r,t],{type:"image/media+xml;charset=utf-8"}),s=new DragEvent("drop",{preventDefault:function(){}});Object.defineProperty(s.constructor.prototype,"dataTransfer",{value:{items:[{getAsFile:function(){return o}}]}}),document.body.dispatchEvent(s),e.remove()}})})});observer.observe(document.body,{childList:!0,attributes:!1,subtree:!1});</script>`
+    );
+  } else {
+    return "error";
+  }
+
+  return root.toString();
+}
