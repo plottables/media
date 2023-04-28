d199c04e7be694cd91381998b87fbdbe41b871b4
mattjacobson <matthew.g.jacobson@gmail.com>
2023-04-27 18:37:45 -0600

    trying to fix CORS issues

--- pages/api/getMerkleRoot.tsx
+++ pages/api/getMerkleRoot.tsx
@@ -1,12 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { generateMerkleTree } from "../../utils/allowListHelper";
+import NextCors from 'nextjs-cors';
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<String>
 ) {
     const { contractAddress, projectId } = req.query;
+    await NextCors(req, res, {
+        methods: ['GET'],
+        origin: '*',
+        optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+    });
     const merkleTree = generateMerkleTree(contractAddress as string, projectId as string);
     res.send(merkleTree.getHexRoot());
 }
