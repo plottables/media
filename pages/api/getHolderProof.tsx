3a9ce25f6d418562e32f570ed9ee57b4839ccdfe
mattjacobson <matthew.g.jacobson@gmail.com>
2024-03-05 11:16:20 -0700

    adding pagination to getHolderProof endpoint

--- pages/api/getHolderProof.tsx
+++ pages/api/getHolderProof.tsx
@@ -16,19 +16,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     .map((a) => a.toLowerCase().trim())
   const networkConfig = getNetworkConfig(chainId as string)
   if (networkConfig) {
-    const alchemy = new Alchemy(networkConfig);
-    const options = {contractAddresses: projects.map((project) => project.split("-")[0])}
-    await alchemy.nft.getNftsForOwner(walletAddress as string, options)
-      .then((data) => {
-        const response = data.ownedNfts
-          .filter((nft) => {
-            return projects.includes(`${nft.contract.address}-${Math.floor(Number(nft.tokenId) / 1000000)}`)
-          })
-          .map((nft) => {
-            return {contractAddress: nft.contract.address, tokenId: nft.tokenId}
-          })[0]
-        res.send(response)
-      });
+    const alchemy = new Alchemy(networkConfig)
+    const uniqueContractAddresses = projects
+      .map((project) => project.split("-")[0])
+      .filter((value, index, array) => array.indexOf(value) === index)
+    const contractAddressesLimit = 20;
+    for (let i = 0; i < uniqueContractAddresses.length; i += contractAddressesLimit) {
+      const contractAddresses = uniqueContractAddresses.slice(i, i + contractAddressesLimit)
+      for await (const nft of alchemy.nft.getNftsForOwnerIterator(walletAddress as string, { contractAddresses })) {
+        if (projects.includes(`${nft.contract.address}-${Math.floor(Number(nft.tokenId) / 1000000)}`)) {
+          res.send({contractAddress: nft.contract.address, tokenId: nft.tokenId})
+        }
+      }
+    }
+    res.send(false)
   } else {
     res.send(false)
   }
